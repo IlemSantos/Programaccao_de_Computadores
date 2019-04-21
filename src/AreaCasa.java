29ae550a28cafcf1d8ccffcda8615e8604b6b8d2
IlemSantos <ilemlima@gmail.com>
2019-04-21 16:53:54 -0400

    commit inicial

---
+++ src/AreaCasa.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**Programa para calcular a área de uma casa(e seus cômodos:uma sala de 10X10m,
+ * um banheiro e um quarto de 5X7m cada.
+ * @author Ilem Santos
+ * @version 1
+ */
+class AreaCasa {
+    public static void main(String[]args){
+        //@version 1
+        System.out.println("@version 1");
+        System.out.println("Programa para cálculo da área da casa");
+        System.out.println("A área da sala é "+10*10);
+        System.out.println("A área do quarto é "+5*7);
+        System.out.println("A área do banheiro é "+5*7);
+        System.out.println("A área total é "+ (10*10 + 5*7 + 5*7));
+   }
+}
