17b65c5de3130a0141a853cf83bcd4b7df3d9cff
IlemSantos <ilemlima@gmail.com>
2019-04-21 18:37:35 -0400

    AreaPiscina@Version1

---
+++ src/AreaPiscina.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**Programa para calcular a área da piscina
+ * @author Ilem Santos
+ */
+public class AreaPiscina {
+        public static void main(String[]args){
+            double raio = 2; //raio da piscina
+            double areap; //área da piscina
+            final double pi = 3.14159; //valor de pi.'final' tornar o valor constante
+            areap = pi*raio*raio;
+            
+        }
+}
