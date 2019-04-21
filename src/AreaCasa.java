bad3952966e2a5e8e54d664337fe7038b2efd33e
IlemSantos <ilemlima@gmail.com>
2019-04-21 17:34:35 -0400

    AreaCasa@Version3

--- src/AreaCasa.java
+++ src/AreaCasa.java
@@ -11,18 +11,26 @@
  */
 class AreaCasa {
     public static void main(String[]args){
-        //@version 2
+        //@version 3
+        /*VANTAGEM?
+        *Reduz nossa dependência a valores extrenos: antes eram 3(10,7e5),agora são 2(10e7)
+        *Reduz a chance de erros na substituição
+        *DESVANTAGEM?
+        *Gasta mais memória,com as variáveis lateral e cquato
+        */
+        int lateral = 10; //comprimento da lateral da cabana
+        int cquarto = 7; //comprimento da lateral maior do quarto
         int areaq; //área do quarto
         int areas; //área da sala
         int areat; //área total
-        System.out.println("\n@version 2");
+        System.out.println("@version 3");
         System.out.println("Programa para cálculo da área da casa");
-        areas = 10*10;
-        System.out.println("A área da sala é "+ areas);
-        areaq = 5*7;
-        System.out.println("A área do quarto é "+ areaq);
-        System.out.println("A área do banheiro é "+ areaq);
+        areas = lateral*lateral;
+        System.out.println("A área da sala é " + areas);
+        areaq = cquarto*(lateral/2);
+        System.out.println("A área do quarto é " +areaq);
+        System.out.println("A área do banheiro é " +areaq);
         areat= areas + 2*areaq;
-        System.out.println("A área total é "+ areat);
+        System.out.println("A área total é " +areat);
    }
 }
