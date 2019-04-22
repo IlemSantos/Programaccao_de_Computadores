98d989412b69a2bb85557be2fb9fdbccbba7760e
IlemSantos <ilemlima@gmail.com>
2019-04-22 13:45:29 -0400

    AreaCasa@Version4

--- src/AreaCasa.java
+++ src/AreaCasa.java
@@ -6,16 +6,13 @@
 
 /**Programa para calcular a área de uma casa(e seus cômodos:uma sala de 11X11m,
  * um banheiro e um quarto de 5.5X7m cada) é uma piscina.
- * Programa com o uso de Métodos.
+ * Programa com o uso de Métodos é Parâmentos.
  * @author Ilem Santos
- * @version 3
+ * @version 4
  */
 class AreaCasa {
-    //Método areaCasa
-    static void areaCasa(){
-        /*O que o significa o void? Que o método não retornar nenhum valor.Ele apenas executa a tarefa e termina.*/ 
-	float lateral = 11;
-	float cquarto = 7;
+    //Método areaCasa com uso parâmetro
+    static void areaCasa(float lateral,float cquarto){ 
 	float areaq;
 	float areas;
 	float areat;
@@ -30,20 +27,17 @@ class AreaCasa {
 	System.out.println("A área total é "+areat);
     }
 
-    //Método areaPiscina
-    static double areaPiscina(){
-        /*O que significa o double? Que o método irá retornar um valor do tipo double.Semelhante ao pow(a,b).*/
-	double raio = 2;
+    //Método areaPiscina agora deve receber um valor(argumento) em seu parâmetro.
+    static double areaPiscina(double raio){
 	return(Math.PI * Math.pow(raio,2));
-        /*E o return? É quando o valor é efetivamente retornado.Asubrotina para aí.*/
     }
          
     public static void main(String[] args){
 	double areap;
 		
-	areaCasa(); //a partir deste ponto,chamar o método areaCasa é executar o método.
+	areaCasa(11,7); //a partir deste ponto,chamar o método areaCasa é executar o método com uso de parâmetro.
 						
-	areap = areaPiscina(); //Note que areaPiscina() retorna valor,então guardamos esse valor em areap
+	areap = areaPiscina(2); //Note que areaPiscina() retorna valor,então guardamos esse valor em areap com uso de parâmetro
 		
 	System.out.println("A área da piscina é "+areap);
    }
