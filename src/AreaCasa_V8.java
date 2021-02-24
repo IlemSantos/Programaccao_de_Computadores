69dda03b404112fc677a5abec8a6bdae78971fae
Ilem Santos <ilemlima@gmail.com>
2021-02-24 07:51:02 -0400

    AreaCasa_V8Final

--- src/AreaCasa_V8.java
+++ src/AreaCasa_V8.java
@@ -6,7 +6,7 @@
 
 /** Programa para calcular a área de uma casa(e seus cômodos: uma sala de 11X11m,
  * um banheiro e um quarto de 5.5X7m cada), piscina e o valor da construção.
- * Programa com o uso de Métodos, Parâmetros, Atributos, Condicionais Aninhados, operador '?', Switch e While aninhado.
+ * Programa com o uso de Métodos, Parâmetros, Atributos, Condicionais Aninhados, operador '?', Switch e While Aninhado.
  *
  * @version 9
  * @author Ilem Santos
