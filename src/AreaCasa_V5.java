45f22060f74fb0bd7826dfbbb7170db03812d3cc
Ilem Santos <ilemlima@gmail.com>
2021-02-23 22:00:44 -0400

    AreaCasa_V5Final

--- src/AreaCasa_V5.java
+++ src/AreaCasa_V5.java
@@ -4,56 +4,64 @@
  * and open the template in the editor.
  */
 
-/**Programa para calcular a área de uma casa(e seus cômodos:uma sala de 11X11m,
- * um banheiro e um quarto de 5.5X7m cada), piscina é o valor da construção.
- * Programa com o uso de Métodos,Parâmetros,Atributos é Condicionais.
+/** Programa para calcular a área de uma casa(e seus cômodos: uma sala de 11X11m,
+ * um banheiro e um quarto de 5.5X7m cada), piscina e o valor da construção.
+ * Programa com o uso de Métodos, Parâmetros, Atributos e Condicionais.
+ *
  * @version 6
  * @author Ilem Santos
  */
 public class AreaCasa_V5 {
+
     static double valorM2 = 1500;//Atributo
-    
+
     //Método areaCasa com uso de parâmetro
-    static void areaCasa(float lateral,float cquarto){
-	float areaq;
-	float areas;
-	float areat;
-		
-	System.out.println("Programa para cálculo da área da casa");
-        areas = lateral*lateral;
+    static void areaCasa(float lateral, float cquarto) {
+        float areaq;
+        float areas;
+        float areat;
+
+        System.out.println("Programa para cálculo da área da casa");
+        areas = lateral * lateral;
         System.out.println("A área da sala é " + areas);
-        areaq = cquarto*(lateral/2);
-        System.out.println("A área do quarto é " +areaq);
-        System.out.println("A área do banheiro é " +areaq);
-        areat= areas + 2*areaq;
-        System.out.println("A área total é " +areat);
+        areaq = cquarto * (lateral / 2);
+        System.out.println("A área do quarto é " + areaq);
+        System.out.println("A área do banheiro é " + areaq);
+        areat = areas + 2 * areaq;
+        System.out.println("A área total é " + areat);
     }
-    
+
     //Método areaPiscina agora deve receber um valor(argumento) em seu parâmetro.
-    static double areaPiscina(double raio){
-        if (raio >= 0) return(Math.PI * Math.pow(raio,2));
-        else return(-1);
-    }
-    
-    //Método valor
-    static double valor(double area){
-        if (area >= 0){
-            return(valorM2*area);
+    static double areaPiscina(double raio) {
+        if (raio >= 0) {
+            return (Math.PI * Math.pow(raio, 2));
+        } else {
+            return (-1);
         }
-        return(-1);//erro -1.
     }
-    
-    public static void main(String[] args){
-	double areap;
+
+    //Método valor
+    static double valor(double area) {
+        if (area >= 0) {
+            return (valorM2 * area);
+        }
+        return (-1);//erro -1.
+    }
+
+    public static void main(String[] args) {
+        double areap;
         double preco;
-					
-	areaCasa(11,7); //a partir deste ponto,chamar o método areaCasa e executar o método com uso de parâmetro
-		
-	areap = areaPiscina(2); //Note que areaPiscina() retorna valor,então guardamos esse valor em areap com uso de parâmetro
-        System.out.println("A área da piscina é "+areap);
-                
+
+        areaCasa(11, 7); //a partir deste ponto,chamar o método areaCasa e executar o método com uso de parâmetro
+
+        areap = areaPiscina(2); //Note que areaPiscina() retorna valor,então guardamos esse valor em areap com uso de parâmetro
+        System.out.println("A área da piscina é " + areap);
+
         preco = valor(20);
-        if (preco >= 0) System.out.println("O valor da construção é "+preco);
-        else System.out.println("Valor da área negativo");
-    } 
+        if (preco >= 0) {
+            System.out.println("O valor da construção é " + preco);
+        } else {
+            System.out.println("Valor da área negativo");
+        }
+    }
 }
