90d7de3b5fe0ae1bea9e6a04ab4f279284240b3c
Ilem Santos <ilemlima@gmail.com>
2021-02-23 20:48:58 -0400

    Area_Casa_V1Final

--- src/AreaCasa_V1.java
+++ src/AreaCasa_V1.java
@@ -4,34 +4,35 @@
  * and open the template in the editor.
  */
 
-/**Programa para calcular a área de uma casa(e seus cômodos:uma sala de 11X11m,
- * um banheiro e um quarto de 5.5X7m cada) é uma piscina.
- * Programa com junção dois programas AreaCasa é AreaPiscina.
+/** Programa para calcular a área de uma casa(e seus cômodos: uma sala de 11X11m,
+ * um banheiro e um quarto de 5.5X7m cada) e uma piscina.
+ * Programa com junção dos programas AreaCasa e AreaPiscina.
+ *
  * @author Ilem Santos
  * @version 2
  */
 class AreaCasa_V1 {
-    public static void main(String[]args){
+
+    public static void main(String[] args) {
 
         float lateral = 11;//comprimento da lateral da cabana alterado
         float cquarto = 7;//comprimento da lateral maior do quarto
         float areaq;//área do quarto
         float areas;//área da sala
         float areat;//área total
-                       
-        System.out.println("Programa para cálculo da área da casa é da piscina.");
-        areas = lateral*lateral;
+
+        System.out.println("Programa para cálculo da área da casa e da piscina.");
+        areas = lateral * lateral;
         System.out.println("A área da sala é " + areas);
-        areaq = cquarto*(lateral/2);
-        System.out.println("A área do quarto é " +areaq);
-        System.out.println("A área do banheiro é " +areaq);
-        areat= areas + 2*areaq;
-        System.out.println("A área total é " +areat);
-        
+        areaq = cquarto * (lateral / 2);
+        System.out.println("A área do quarto é " + areaq);
+        System.out.println("A área do banheiro é " + areaq);
+        areat = areas + 2 * areaq;
+        System.out.println("A área total é " + areat);
+
         double raio = 2; //raio da piscina
         double areap; //área da piscina
-        areap = Math.PI*Math.pow(raio,2);
-        System.out.println("A área da piscina é " +areap);
-        }   
+        areap = Math.PI * Math.pow(raio, 2);
+        System.out.println("A área da piscina é " + areap);
     }
-        
+}
