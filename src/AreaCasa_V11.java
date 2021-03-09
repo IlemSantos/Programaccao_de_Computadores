6f8dc76aeaa9350dfb549cffba7c8f34271ecec1
Ilem Santos <ilemlima@gmail.com>
2021-03-08 23:27:02 -0400

    AreaCasa_V11Final

--- src/AreaCasa_V11.java
+++ src/AreaCasa_V11.java
@@ -8,10 +8,10 @@
  * um banheiro e um quarto de 5.5X7m cada), piscina e o valor da construção.
  * Programa com o uso de Métodos, Parâmetros, Atributos, Condicionais, operador '?', For Aninhado e Matriz.
  *
- * @version 12
+ * @version 13
  * @author Ilem Santos
  */
-public class AreaCasa_V11 {
+public class AreaCasa_V12 {
 
     /* valor do metro quadrado da casa */
     static double valorM2 = 1500;
@@ -74,16 +74,26 @@ public class AreaCasa_V11 {
         }
     }
 
+    /* Retorna matriz com os preços finais. */
+    public static double[][] calculaFinal(double[][] val, double[][] desc) {
+        double[][] saida = new double[val.length][val[0].length];
+        for (int i = 0; i < saida.length; i++) {
+            for (int j = 0; j < saida[0].length; j++) {
+                saida[i][j] = val[i][j] * (1 - desc[i][j]);
+            }
+        }
+        return (saida);
+
+    }
+
     public static void main(String[] args) {
         double[][] valores = new double[4][4];
+        double[][] descontos = {{0, 0, 0.2, 0.2}, {0.05, 0.05, 0.1, 0.15}, {0.02, 0.04, 0.08, 0.15}, {0, 0, 0, 0.05}};
         double areap;
         double preco;
         double media = 0;
 
         areaCasa(11, 7); //a partir deste ponto, chamar o método areaCasa e executar o método com uso de parâmetro
-
-        areap = areaPiscina(2); //Note que areaPiscina() retorna valor, então guardamos esse valor em areap com uso de parâmetro
-        System.out.println("A área da piscina é " + areap);
 
         preco = valorCasa(20);
         if (preco >= 0) {
@@ -92,15 +102,22 @@ public class AreaCasa_V11 {
             System.out.println("Valor da área negativo");
         }
 
+        areap = areaPiscina(2); //Note que areaPiscina() retorna valor, então guardamos esse valor em areap com uso de parâmetro
+        System.out.println("A área da piscina é " + areap);
+
         for (double valor : precos) {
             media += valor;
         }
         media = media / precos.length;
         System.out.println("O preço médio dos materiais de construção da piscina é " + media);
 
+        double[][] pFinal;
+
         carregaVal(valores);
 
-        for (double[] linha : valores) {
+        pFinal = calculaFinal(valores, descontos);
+
+        for (double[] linha : pFinal) {
             for (double valor : linha) {
                 System.out.print(valor + "  ");
             }
