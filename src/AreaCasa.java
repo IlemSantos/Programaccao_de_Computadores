a2cb71f495827b79a00ed3579d5ee7c77524abd4
Ilem Santos <ilemlima@gmail.com>
2021-03-15 08:25:27 -0400

    Classes

--- src/AreaCasa.java
+++ src/AreaCasa.java
@@ -1,103 +1,32 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/** Programa para calcular a área de uma casa(e seus cômodos: uma sala de 11X11m,
- * um banheiro e um quarto de 5.5X7m cada), piscina é o valor da construção.
- * Programa com o uso de Métodos,Parâmentos,Atributos,Condicionais Aninhados,operador '?' e Switch.
- *
- * @author Ilem Santos
- * @version 8
- */
 public class AreaCasa {
 
-    static double valorM2 = 1500;//Atributo
-    static final int ALVENARIA = 0;
-    static final int VINIL = 1;
-    static final int FIBRA = 2;
-    static final int PLASTICO = 3;
+    double valorM2;
 
-    //Método areaCasa com uso de parâmetro
-    static void areaCasa(float lateral, float cquarto) {
+    /* Construtor que chama outro constrtor */
+    AreaCasa() {
+        this(1500.0); // chamar outro construtor com parâmetro
+    }
+
+    /* Construtor padrão */
+    AreaCasa(double valorM2) {
+        this.valorM2 = valorM2;
+    }
+
+
+    /* Calcula a área da casa */
+    double area(double lateral, double cquarto) {
+        double areat = -1;    // área total
         if (lateral >= 0 && cquarto >= 0) {
-            float areaq;
-            float areas;
-            float areat;
+            areat = lateral * lateral;
+            areat += cquarto * lateral;
 
-            System.out.println("Programa para cálculo da área da casa e da piscina");
-            areas = lateral * lateral;
-            System.out.println("A área da sala é " + areas);
-            areaq = cquarto * (lateral / 2);
-            System.out.println("A área do quarto é " + areaq);
-            System.out.println("A área do banheiro é " + areaq);
-            areat = areas + 2 * areaq;
-            System.out.println("A área total é " + areat);
-        } else {
-            System.out.println("Erro: Parâmetro");
         }
+        return (areat);
     }
 
-    //Método areaPiscina agora deve receber um valor(argumento) em seu parâmetro.
-    static double areaPiscina(double raio) {
-        return (raio >= 0) ? (Math.PI * Math.pow(raio, 2)) : (-1);//O operador '?'
-    }
-
-    //Método valor
-    static double valor(double area) {
+    /* Calcula o valor total da construção */
+    double valor(double area) {
         return ((area >= 0) ? (valorM2 * area) : (-1));//O operador '?'
-    }
-
-    /**
-     * Método valorPiscina para calcular custo da construção da piscina. O
-     * método foi utilizado o Switch.
-     */
-    static double valorPiscina(double area, int material) {
-        double valor;
-        switch (material) {
-            case ALVENARIA:
-                valor = 1500;
-                break;
-            case VINIL:
-                valor = 1100;
-                break;
-            case FIBRA:
-                valor = 750;
-                break;
-            case PLASTICO:
-                valor = 500;
-                break;
-            default:
-                valor = -1;
-        }
-        return (area * valor);
-    }
-
-    public static void main(String[] args) {
-        double areap;
-        double preco;
-        double area = 100;
-        int tipo = ALVENARIA;
-
-        areaCasa(11, 7); //a partir deste ponto,chamar o método areaCasa é executar o método com uso de parâmetro.
-
-        areap = areaPiscina(2); //Note que areaPiscina() retorna valor,então guardamos esse valor em areap com uso de parâmetro
-        System.out.println("A área da piscina é " + areap);
-
-        preco = valor(20);
-        if (preco >= 0) {
-            System.out.println("O valor da construção é " + preco);
-        } else {
-            System.out.println("Valor da área negativo");
-        }
-
-        System.out.println("Valor da construção vinil " + valorPiscina(100, 1));
-
-        System.out.println("Material \t\tValor");
-        while (tipo <= PLASTICO) {
-            System.out.println(tipo + "\t\t" + valorPiscina(area, tipo));
-            tipo++;
-        }
     }
 }
--- src/AreaPiscina.java
+++ src/AreaPiscina.java
@@ -1,17 +1,50 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**Programa para calcular a área da piscina
- * @author Ilem Santos
- */
 public class AreaPiscina {
-    public static void main(String[]args){
-        double raio = 2; //raio da piscina
-        double areap; //área da piscina
-        areap = Math.PI*Math.pow(raio,2);
-        System.out.println("A área da piscina é " +areap);
+
+    /* materiais da piscina */
+    static final int ALVENARIA = 0;
+    static final int VINIL = 1;
+    static final int FIBRA = 2;
+    static final int PLASTICO = 3;
+
+    /* preços dos materiais para construção da piscina */
+    static double[] precos = {1500, 1100, 750, 500};
+
+    /* nomes dos materiais da piscina. */
+    static char[][] nomes = {{'A', 'l', 'v', 'e', 'r', 'n', 'a', 'r', 'i', 'a'}, {'V', 'i', 'n', 'i', 'l'}, {'F', 'i', 'b', 'r', 'a'}, {'P', 'l', 'a', 's', 't', 'i', 'c', 'o'}};
+
+    /* Calcula a área da piscina */
+    static double area(double raio) {
+        return (raio >= 0) ? (Math.PI * Math.pow(raio, 2)) : (-1);//O operador '?'
     }
+
+    /* Calcula o valor da construção da piscina */
+    static double valor(double area, int material) {
+        if (material < ALVENARIA || material > PLASTICO || area < 0) {
+            return (-1);
+        }
+        return (area * precos[material]);
+    }
+
+    /* Carrega os valores das piscinas na matriz de área X material. */
+    public static void carregaVal(double[][] m) {
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 50; j <= 200; j += 50) {
+                m[i][j / 50 - 1] = valor(j, i);
+            }
+        }
+    }
+
+    /* Retorna matriz com os preços finais. */
+    public static double[][] calculaFinal(double[][] val, double[][] desc) {
+        double[][] saida = new double[val.length][val[0].length];
+        for (int i = 0; i < saida.length; i++) {
+            for (int j = 0; j < saida[0].length; j++) {
+                saida[i][j] = val[i][j] * (1 - desc[i][j]);
+            }
+        }
+        return (saida);
+
+    }
+
 }
